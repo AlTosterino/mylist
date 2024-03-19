1665b12d6c63f718183eadc6b394cccd366cdf97
Daniel Różycki <daniel.rozycki@stxnext.pl>
2024-03-19 13:22:58 +0100

    :sparkles: Check if serverless API is working on Vercel

---
+++ api/hello.ts
@@ -0,0 +1,7 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+
+export default function handler(request: VercelRequest, response: VercelResponse) {
+    const { name = 'World' } = request.query;
+    return response.send(`Hello ${name}!`);
+}
